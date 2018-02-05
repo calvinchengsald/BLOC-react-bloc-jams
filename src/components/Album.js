9a1f2d55f3ceb241dad9dceca91b5a65675697f4
Calvin <calvin.cheng@stonybrook.edu>
2018-02-05 17:09:50 -0500

    mod2.2.7

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,8 +11,45 @@ import albumData from './../data/albums'
      });
 
      this.state ={
-       currentAlbum : currentAlbum
+       currentAlbum : currentAlbum,
+       currentSong : currentAlbum.songs[0],
+       isPlaying: false
      }
+
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = this.state.currentAlbum.songs[0].audioSrc;
+   }
+
+   play(){
+     this.audioElement.play();
+     this.setState({
+       isPlaying : true
+     });
+   }
+   pause(){
+     this.audioElement.pause();
+     this.setState({
+       isPlaying : false
+     });
+   }
+   setSong(songToBe){
+     this.setState({
+       currentSong : songToBe
+     })
+     this.audioElement.src= songToBe.audioSrc;
+   }
+   handleClickSong(song){
+     const isSameSong = (song === this.state.currentSong);
+     if(isSameSong && this.state.isPlaying){
+       this.pause();
+       return;
+     }
+     if(isSameSong && !this.state.isPlaying){
+       this.play();
+       return;
+     }
+     this.setSong(song);
+     this.play();
    }
 
    render() {
@@ -25,7 +62,6 @@ import albumData from './../data/albums'
             <h1 className='album-details-title'> Title: {this.state.currentAlbum.title} </h1>
             <h2 className='album-details-artist'> Artist: {this.state.currentAlbum.artist} </h2>
             <div className='album-details-release-info'> Release Info: {this.state.currentAlbum.releaseInfo} </div>
-
             <table id='song-list'>
               <colgroup>
                 <col id='song-number-column'/>
@@ -39,14 +75,27 @@ import albumData from './../data/albums'
               </tr>
               <tbody>
                 {this.state.currentAlbum.songs.map( (song, index)=>
-                  <tr>
-                    <td> {index +1} </td>
+                  <tr className = 'song' key={index} onClick={()=>this.handleClickSong(song)}>
+                    <td>
+                        <button>
+                          <span className="song-number">{index+1}</span>
+                          <span className="ion-play"></span>
+                          <span className="ion-pause"></span>
+                       </button>
+                    </td>
                     <td> {song.title} </td>
                     <td> {song.duration} </td>
                   </tr>
                 )}
               </tbody>
             </table>
+            <div>
+              [Debug info] Current Song: {this.state.currentSong.title}
+            </div>
+
+            <div>
+              [Debug info] Current Src: {this.audioElement.src}
+            </div>
 
           </div>
         </section>
